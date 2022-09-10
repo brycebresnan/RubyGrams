8befa8e085f3ea7284a551cd96c0419c5ba47ec1
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-09-09 23:26:44 -0700

    GREEN sentence test and fix responses

--- lib/rubygrams.rb
+++ lib/rubygrams.rb
@@ -15,20 +15,20 @@ class Grams
   end
 
   def anagram?
-    array_1 = @string_1.downcase.split(//)
-    array_2 = @string_2.downcase.split(//)
+    array_1 = @string_1.gsub(/[^A-Za-z]/, '').downcase.split(//)
+    array_2 = @string_2.gsub(/[^A-Za-z]/, '').downcase.split(//)
 
     if (!self.class.word?(array_1) || !self.class.word?(array_2))
       return "You need to input actual words!"
     end
 
     if (array_1.all? { |letter| array_2.include?(letter)})
-      return "These words are anagrams."
+      return "These examples are anagrams."
     elsif (array_1.none? { |letter| array_2.include?(letter)})
-      return "These words have no letter matches...but they are antigrams!"
+      return "These examples have no letter matches...but they are antigrams!"
     else
-      return "Sorry! These words are not anagrams, nor are they antigrams"
+      return "Sorry! These examples are not anagrams, nor are they antigrams"
     end
   end
-
+  
 end
--- spec/rubygrams_spec.rb
+++ spec/rubygrams_spec.rb
@@ -4,17 +4,17 @@ require ("./lib/rubygrams.rb")
 describe('#anagram?') do
   it("should return true if two words are found to be anagrams when compared") do
   test_words = Grams.new("race","care")
-  expect(test_words.anagram?).to(eq("These words are anagrams."))
+  expect(test_words.anagram?).to(eq("These examples are anagrams."))
   end
 
   it("should return false if two words are found not to be anagrams when compared") do
     test_words = Grams.new("bace","acae")
-    expect(test_words.anagram?).to(eq("Sorry! These words are not anagrams, nor are they antigrams"))
+    expect(test_words.anagram?).to(eq("Sorry! These examples are not anagrams, nor are they antigrams"))
     end
 
   it("should still match anagrams despite casing differences") do
     test_words = Grams.new("Race","cAre")
-    expect(test_words.anagram?).to(eq("These words are anagrams."))
+    expect(test_words.anagram?).to(eq("These examples are anagrams."))
     end
 
   it("should check if inputted strings are actual words. (Rule 1: contains a vowel") do
@@ -24,12 +24,12 @@ describe('#anagram?') do
 
   it("if strings are not antigrams (false), check to see if no letters are in common.") do
     test_words = Grams.new("dog","cat")
-    expect(test_words.anagram?).to(eq("These words have no letter matches...but they are antigrams!"))
+    expect(test_words.anagram?).to(eq("These examples have no letter matches...but they are antigrams!"))
     end
 
   it("should account for sentences that are anagrams.") do
     test_words = Grams.new("The Morse Code.","Here come dots!")
-    expect(test_words.anagram?).to(eq("These words are anagrams."))
+    expect(test_words.anagram?).to(eq("These examples are anagrams."))
     end
 
 end
