79ef8ac237c761b1886e8562bef42a87a4ef7ab9
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-09-09 22:36:11 -0700

    add word? function and refactor downcase method

--- lib/rubygrams.rb
+++ lib/rubygrams.rb
@@ -5,14 +5,23 @@ class Grams
   attr_accessor(:string_2)
 
   def initialize(string_1, string_2)
-    @string_1 = string_1.downcase
-    @string_2 = string_2.downcase
+    @string_1 = string_1
+    @string_2 = string_2
+  end
+
+  def self.word?(array)
+    vowels_array = ['a','e','i','o','u']
+    array.any? { |letter| vowels_array.include?(letter)}
   end
 
   def anagram?
-    array_1 = @string_1.split(//)
-    array_2 = @string_2.split(//)
+    array_1 = @string_1.downcase.split(//)
+    array_2 = @string_2.downcase.split(//)
+
+    if (!self.class.word?(array_1) || !self.class.word?(array_2))
+      return nil
+    end
+
     array_1.all? { |letter| array_2.include?(letter)} 
   end
-
 end
