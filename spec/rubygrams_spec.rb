38905c43e17b5bfbd392c2908a8be956511d723c
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-09-10 21:06:48 -0700

    RED test

--- spec/rubygrams_spec.rb
+++ spec/rubygrams_spec.rb
@@ -8,7 +8,7 @@ describe('#anagram?') do
   end
 
   it("should return false if two words are found not to be anagrams when compared, but returns any matching letters") do
-    test_words = Grams.new("bottle","top")
+    test_words = Grams.new("bot","top")
     expect(test_words.anagram?).to(eq("Sorry! These examples are not anagrams, but 2 letters match: o, t"))
     end
 
@@ -30,6 +30,11 @@ describe('#anagram?') do
   it("should account for sentences that are anagrams.") do
     test_words = Grams.new("The Morse Code.","Here come dots!")
     expect(test_words.anagram?).to(eq("These examples are anagrams."))
+    end
+    
+  it("should make sure the lengths of the examples are the same. If not, return an error") do
+    test_words = Grams.new("looks","kool")
+    expect(test_words.anagram?).to(eq("These examples are not the same number of characters, thus they can not be anagrams."))
     end
 
 end
